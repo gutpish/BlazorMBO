54f42e56c9796285218c2f3d30dff171485f1fe6
Anthony <anthonyannibale@hotmail.fr>
2020-10-13 20:40:02 +0200

    Ajout des classes Tournois, Area..

---
+++ BlazorMBO/BlazorMBO/Shared/Classes/Saison.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMBO.Shared
+{
+    public class Saison
+    {
+        public int SeasonId { get; set; }
+        public int CompetitionId { get; set; }
+        public int Season { get; set; }
+        public string Name { get; set; }
+        public string CompetitionName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool CurrentSeason { get; set; }
+        public List<Round> Rounds { get; set; }
+    }
+}
